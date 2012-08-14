9d374eec065381b5f53806cb8991a9a2d318760f
Marcel <marcel.kalveram@googlemail.com>
2012-08-15 01:30:43 +0200

    IE7 autogrow tests

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ window.fbAsyncInit = function() {
     // Initialize the FB javascript SDK
     FB.init({
 
-        appId   : 'APP_ID', // APP ID muss durch ID der aktuellen Anwendung ersetzt werden
+        appId   : '269633396481423', // APP ID muss durch ID der aktuellen Anwendung ersetzt werden
         status  : true,
         cookie  : true,
         xfbml   : true
 
     });
 
-    // Auto grows panel when app is higher than 800px. 
+    // Auto grows panel when app is higher than 800px.
     // In the app settings, height needs to be set to height 800px fixed for this to work
     FB.Canvas.setAutoGrow();
 
-}
+};
